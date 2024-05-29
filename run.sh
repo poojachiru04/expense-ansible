29ac3f1acd29938473d55b2f91eab98b16179886
pooja <poojaskumara@gmail.com>
2024-05-29 17:19:08 +0530

    dry code in gocd

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ "$pipeline" == "true" ]; then
   exit
 fi
 ##
-ansible-playbook -i ${component}-${env}.rdevopsb79.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=${env} expense.yml
+ansible-playbook -i ${component}-${env}.poodevops.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=${env} expense.yml
