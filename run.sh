371b940442005b876a6e5b9e7c78038ae56be456
pooja <poojaskumara@gmail.com>
2024-05-29 19:59:35 +0530

    gocd pipelines

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i ${Component}-${env}.poodevops.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${Component} -e env=${env} expense.yml
+ansible-playbook -i ${component}-${env}.poodevops.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=${env} expense.yml
