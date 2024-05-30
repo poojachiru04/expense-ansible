36b9e1b93aa96228a692a06c381cbeacb9381a79
pooja <poojaskumara@gmail.com>
2024-05-30 09:53:28 +0530

    templates for pipelines

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
+## For template pipeline we will skip the ansible run
+if [ "$pipeline" == "true" ]; then
+  exit
+fi
+##
 ansible-playbook -i ${component}-${env}.poodevops.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=${env} expense.yml
